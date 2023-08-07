ecb5479c45c1eb52026c1dd3904e0adb7f5d5d56
Chris Bentley <chrisrbentley@pm.me>
2023-08-07 00:15:02 -0400

    serve pages according to url

---
+++ index.js
@@ -0,0 +1,40 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+http
+	.createServer((req, res) => {
+		const parsedUrl = url.parse(req.url, true);
+		const path = parsedUrl.pathname;
+		console.log(path);
+
+		const filename = `${path}`;
+		console.log('filename is ' + filename);
+
+		if (filename === '/') {
+			fs.readFile('./index.html', (err, data) => {
+				if (err) {
+					res.writeHead(404, { 'Content-Type': 'text/html' });
+					return res.end('404 Not Found');
+				}
+				res.writeHead(200, { 'Content-Type': 'text/html' });
+				res.write(data);
+				return res.end();
+			});
+		} else {
+			fs.readFile(`.${filename}.html`, (err, data) => {
+				if (err) {
+					fs.readFile('./404.html', (err, data) => {
+						res.writeHead(200, { 'Content-Type': 'text/html' });
+						res.write(data);
+					});
+				}
+
+				// Send a response
+				res.writeHead(200, { 'Content-Type': 'text/html' });
+				res.write(data);
+				return res.end();
+			});
+		}
+	})
+	.listen(8080);
